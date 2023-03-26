6e59e1bb2072685b689c255429f17f42839cdd1c
Aninha <aninhapss2000@gmail.com>
2023-03-26 18:32:15 -0300

    Added Request e closure

---
+++ ProjectClosureFive/ProjectClosureFive/RequestPizza.swift
@@ -0,0 +1,20 @@
+//
+//  RequestPizza.swift
+//  ProjectClosureFive
+//
+//  Created by Ana Paula Silva de Sousa on 26/03/23.
+//
+
+import UIKit
+import Alamofire
+
+class RequestPizza: NSObject {
+
+    func requestPizza(completion: @escaping (Pizza?) -> Void ){
+        AF.request("https://p3teufi0k9.execute-api.us-east-1.amazonaws.com/v1/pizza", method: .get).response { response in
+            let pizza = try? JSONDecoder().decode(Pizza.self, from: response.data ?? Data())
+            
+            completion(pizza)
+        }
+    }
+}
