871517494132167ab5e1478f508f99bba2f6c46b
Aninha <aninhapss2000@gmail.com>
2023-03-26 18:51:42 -0300

    Added datasource e register da xib

--- ProjectClosureFive/ProjectClosureFive/ViewController.swift
+++ ProjectClosureFive/ProjectClosureFive/ViewController.swift
@@ -21,9 +21,27 @@ class ViewController: UIViewController {
     }
     
     func setupTableView() {
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MyCustomCellXIB", bundle: nil), forCellReuseIdentifier: "cellXIB")
         request.requestPizza { pizza in
             self.arraypizza = pizza
             self.tableView.reloadData()
         }
+    }
+}
+
+extension ViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arraypizza?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cellXIB", for: indexPath) as? MyCustomCellXIB {
+            cell.setupXIB(pizza: arraypizza?[indexPath.row])
+            
+            return cell
+        }
+        
+        return UITableViewCell()
     }
 }
